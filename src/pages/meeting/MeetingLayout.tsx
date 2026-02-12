0ab37df576d381cdd04684e3b9eb416ca96a825a
dkr-sjr <rkdrjs4637@naver.com>
2026-02-13 02:09:33 +0900

    feat : MeetingLayout에 상태 관리 로직 작성

--- src/pages/meeting/MeetingLayout.tsx
+++ src/pages/meeting/MeetingLayout.tsx
@@ -1,11 +1,62 @@
-import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
+import { Outlet, useOutletContext } from 'react-router-dom';
+
+import { useGetMeetingDetail } from '@/hooks/useMeeting';
+import { useParticipantList } from '@/hooks/useParticipant';
+
+import { type ParticipantList } from '@/types/meetingTypes';
+
+export interface MeetingOutletContext {
+  //서버
+  meetingName: string;
+  isTimeRecommendEnabled: boolean;
+  isPlaceRecommendEnabled: boolean;
+  dateType: string;
+  timeRange: string;
+  participantStatusList: ParticipantList | undefined;
+
+  //클라
+  nickName: string;
+  setNickName: React.Dispatch<React.SetStateAction<string>>;
+  id: string;
+  setId: React.Dispatch<React.SetStateAction<string>>;
+
+  // 3. 로딩 상태
+  isLoading: boolean;
+}
 
 export default function MeetingLayout() {
-  const [temp, setTemp] = useState('');
+  const { data: meetingData, isLoading: isMeetingLoading } = useGetMeetingDetail();
+  const { data: participantData, isLoading: isParticipantLoading } = useParticipantList();
+
+  const [nickName, setNickName] = useState('');
+  const [id, setId] = useState('');
+
+  const contextValue: MeetingOutletContext = {
+    meetingName: meetingData?.meetingName || '',
+    isTimeRecommendEnabled: meetingData?.isTimeRecommendEnabled || false,
+    isPlaceRecommendEnabled: meetingData?.isPlaceRecommendEnabled || false,
+    dateType: meetingData?.dateType || '',
+    timeRange: meetingData?.timeRange || '',
+
+    participantStatusList: participantData?.participantList,
+
+    nickName,
+    setNickName,
+    id,
+    setId,
+
+    isLoading: isMeetingLoading || isParticipantLoading,
+  };
+
   return (
     <div>
-      <Outlet context={{temp, setTemp}} />
+      <Outlet context={contextValue} />
     </div>
-  )
+  );
+}
+
+// 커스텀 훅
+export const useMeetingContext = () => {
+  return useOutletContext<MeetingOutletContext>();
 };
