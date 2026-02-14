ae8c2de1a511e41473b658c42901afaeb95166f0
dkr-sjr <rkdrjs4637@naver.com>
2026-02-14 18:24:10 +0900

    feat : StartPage 간단 제작

--- src/pages/landing/StartPage.tsx
+++ src/pages/landing/StartPage.tsx
@@ -1,7 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+
+import { AppLayout } from '@/components/common/layout/AppLayout';
+import { FixedBottomButton } from '@/components/common/layout/FixedBottomButton';
+
 export default function StartPage() {
+  const navigate = useNavigate();
+
+  const handleStartClick = () => {
+    navigate('create');
+  };
+
   return (
-    <div>
-      start
-    </div>
-  )
-};
+    <AppLayout
+      header={''}
+      pageBackgroundClassName="bg-gray-100/70"
+      bottom={
+        <div className="flex items-center pt-2">
+          <FixedBottomButton
+            className="bg-greedy hover:bg-greedy/50 mx-auto h-17 w-95/100 rounded-4xl text-xl"
+            onClick={handleStartClick}
+          >
+            시작하기
+          </FixedBottomButton>
+        </div>
+      }
+    >
+      <div className="mt-40 flex flex-col gap-5">
+        <div className="text-center text-4xl font-bold">
+          결정 안되던 약속, <br />
+          여기서 끝내세요
+        </div>
+        <div className="text-center text-gray-400">
+          모두의 시간과 위치를 고려해 <br />
+          가장 납득이 되는 약속을 만들어 드릴게요
+        </div>
+      </div>
+    </AppLayout>
+  );
+}
