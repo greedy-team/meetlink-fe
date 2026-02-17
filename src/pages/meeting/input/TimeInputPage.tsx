94dc6b5e4f7b54c411f0c4d6d97aba49333139f8
dkr-sjr <rkdrjs4637@naver.com>
2026-02-17 20:30:25 +0900

    chore : date-fn 패키지 추가

--- src/pages/meeting/input/TimeInputPage.tsx
+++ src/pages/meeting/input/TimeInputPage.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+import { AppLayout } from '@/components/common/layout/AppLayout';
+import { FixedBottomButton } from '@/components/common/layout/FixedBottomButton';
+import { Header } from '@/components/common/layout/Header';
 import { useUpdateMyAvailableTime } from '@/hooks/useTime';
 
 import { useMeetingContext } from '@/pages/meeting/MeetingLayout';
@@ -20,9 +23,16 @@ export default function TimeInputPage() {
   };
 
   return (
-    <div>
-      <div></div>
-      timeinput
-    </div>
+    <AppLayout
+      header={<Header title="가능 시간 선택" showBackButton={true} showSettingButton={false} />}
+      pageBackgroundClassName="bg-gray-100/70"
+      bottom={
+        <div className="space-y-3">
+          <FixedBottomButton>저장하기</FixedBottomButton>
+        </div>
+      }
+    >
+      <div className="space-y-4"></div>
+    </AppLayout>
   );
 }
