a943b1f994018ff2b5f43e6312c34ee238feae1c
dkr-sjr <rkdrjs4637@naver.com>
2026-02-23 14:49:31 +0900

    refactor : 테스트용 주소 변경

--- vite.config.ts
+++ vite.config.ts
@@ -21,14 +21,13 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
 
-        // 🚨 구조 수정됨: configure를 '/api' 내부로 이동했습니다!
         configure: (proxy, _options) => {
           proxy.on('proxyReq', (proxyReq, _req, _res) => {
             // 외부 IP(172.x.x.x)로 접속해도 백엔드에게는 localhost에서 온 것처럼 속입니다.
             proxyReq.setHeader('Origin', 'http://localhost:5173');
           });
         },
-      } as ProxyOptions, // 타입스크립트 에러 방지를 위한 타입 명시
+      } as ProxyOptions,
     },
   },
 });
