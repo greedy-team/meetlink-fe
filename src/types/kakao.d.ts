c79a14e0b72828036a0071abc2ff1bdf3da98d05
Yeryeong Kang <dpfud8786@gmail.com>
2026-02-23 15:41:30 +0900

    chore: kakao.d.ts 파일 수정

--- src/types/kakao.d.ts
+++ src/types/kakao.d.ts
@@ -1,22 +1,38 @@
 export {};
 
 declare global {
+  type KakaoLatLng = {
+    getLat(): number;
+    getLng(): number;
+  };
+
+  type KakaoMap = {
+    setCenter(pos: KakaoLatLng): void;
+    getCenter(): KakaoLatLng;
+    relayout?(): void;
+  };
+
+  type KakaoMarker = {
+    setMap(map: KakaoMap | null): void;
+    setPosition(pos: KakaoLatLng): void;
+  };
+
   interface Window {
     kakao: {
       maps: {
         load(cb: () => void): void;
 
-        LatLng: new (lat: number, lng: number) => unknown;
+        LatLng: new (lat: number, lng: number) => KakaoLatLng;
+
         Map: new (
           container: HTMLElement,
-          options: { center: unknown; level: number },
-        ) => {
-          setCenter(pos: unknown): void;
-          relayout?(): void;
-        };
-        Marker: new (options: { position: unknown }) => {
-          setMap(map: unknown | null): void;
-          setPosition(pos: unknown): void;
+          options: { center: KakaoLatLng; level: number },
+        ) => KakaoMap;
+
+        Marker: new (options: { position: KakaoLatLng }) => KakaoMarker;
+
+        event: {
+          addListener(target: unknown, type: string, handler: () => void): void;
         };
       };
     };
