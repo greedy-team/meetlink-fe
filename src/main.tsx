bb07bd3fce01d34ba434a71c6e6d0c42396085e7
dkr-sjr <rkdrjs4637@naver.com>
2026-02-25 21:13:33 +0900

    feat : sonner 사용 준비

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import { Toaster } from '@/components/ui/sonner';
+
 import App from './App.tsx';
 
 import './index.css';
@@ -21,6 +23,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Toaster richColors closeButton position="top-center" />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
