9fd64fcbf5bf4c76544c05d5430a10f961aced4e
dkr-sjr <rkdrjs4637@naver.com>
2026-04-07 15:19:47 +0900

    style : button border style 변경

--- src/pages/meeting/JoinPage.tsx
+++ src/pages/meeting/JoinPage.tsx
@@ -234,7 +234,7 @@ export default function JoinPage() {
             disabled={!canSubmit || isPending}
             loading={isPending}
             onClick={onSubmit}
-            className="bg-greedy hover:bg-greedy/50 border-greedy-strong border-2"
+            className="bg-greedy hover:bg-greedy/50"
           >
             참여하기
           </FixedBottomButton>
